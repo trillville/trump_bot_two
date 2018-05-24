c30e72298b112a638671f200d1b0be5af3640e80
trillville <tillman.elser@gmail.com>
2018-05-23 23:43:20 -0700

    switch to real tweets

--- trump_tweeter.py
+++ trump_tweeter.py
@@ -3,6 +3,8 @@ import json
 import sys
 import numpy as np
 import twitter
+import os
+import psycopg2
 from keras.models import load_model
 
 NUM_CHARS = 61
@@ -27,7 +29,7 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 # What is trump gonna say next!
-def generate_output_tweet(seed_tweet):
+def generate_output_tweet(seed_tweet, char_indices, indices_char):
     sentence_trunc = seed_tweet[-MAX_LEN:]
     generated = ''
     next_char = ''
@@ -46,7 +48,36 @@ def generate_output_tweet(seed_tweet):
         sentence_trunc = sentence_trunc[1:] + next_char
     return(generated)
 
+def get_new_tweets(conn, api):
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS last_tweet_db (tweet_id BIGINT);")
+    cur.execute("SELECT tweet_id FROM last_tweet_db")
+    last_tweet = cur.fetchone()
+    api_tweets = api.GetUserTimeline(screen_name="realDonaldTrump")
+    if last_tweet is None or last_tweet != api_tweets[0].id:
+        cur.execute("INSERT INTO last_tweet_db VALUES (%s)", (api_tweets[0].id,))
+        input_tweet = api_tweets[0].text
+        i = 1
+        while len(input_tweet) < 200:
+            input_tweet = api_tweets[i].text + input_tweet
+            i += 1
+        return input_tweet
+    else:
+        print("NO NEW TWEETS")
+        sys.exit(1)
+
 def main():
+    # Connect to Twitter API
+    api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'),
+                      consumer_secret=os.environ.get('CONSUMER_KEY_SECRET'),
+                      access_token_key=os.environ.get('ACCESS_TOKEN'),
+                      access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET'))
+
+    # Connect to Postgres
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    latest_tweet = get_new_tweets(conn, api)
+
     # Load character dictionaries
     char_indices = json.load(open('char_indices.json'))
     indices_char = json.load(open('indices_char.json'))
@@ -54,21 +85,10 @@ def main():
     # Load model
     model = load_model('model.h5')
 
-    # Static tweet for testing
-    tweet_text = "California finally deserves a great Governor one who understands borders crime and lowering taxes. John Cox is the man - he’ll be the best Governor you’ve ever had. I fully endorse John Cox for Governor and look forward to working with him to Make California Great Again!"
-
-    api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'),
-                      consumer_secret=os.environ.get('CONSUMER_KEY_SECRET'),
-                      access_token_key=os.environ.get('ACCESS_TOKEN'),
-                      access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET'))
-
     # Generate tweet
-    tweet_formatted = preprocess_input_tweet(tweet_text)
-    generate_output_tweet(tweet_formatted)
+    tweet_formatted = preprocess_input_tweet(latest_tweet)
+    output_tweet = generate_output_tweet(tweet_formatted, char_indices, indices_char)
 
-    # Generate tweet
-    tweet_formatted = preprocess_input_tweet(tweet_text)
-    output_tweet = generate_output_tweet(tweet_formatted)
     # Post tweet
     api.PostUpdate(output_tweet)
 
