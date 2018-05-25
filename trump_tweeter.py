b47f1d9af32197da2544cfd5f197f79f1c396466
trillville <tillman.elser@gmail.com>
2018-05-25 16:56:54 -0700

    1:1 bot:trump tweet ratio git status :

--- trump_tweeter.py
+++ trump_tweeter.py
@@ -9,22 +9,32 @@ import random
 from keras.models import load_model
 
 NUM_CHARS = 61
-MAX_LEN = 140
+MODEL_INPUT_LEN = 140
 CHARACTER_LIMIT = 279
 POSSIBLE_HOURS = ['7','8','9']
 POSSIBLE_MINUTES = ['00','15','30','45']
-DIVERSITY_RANGE = [0.15,0.35]
+MIN_TEMP = 0.20
+MAX_TEMP = 0.40
+BASE_SEED_TWEET = '~While in the Philippines I was forced to watch @CNN which I have not done in months and again realized how bad and FAKE it is. Loser!`'
 
-# Hacky regex... but cleans up the text a little bit
+# Clean up input tweet
 def preprocess_input_tweet(tweet_text):
     t1 = re.sub(r'[^A-Za-z@!.?~`:/ ]', '', tweet_text)
     t2 = re.sub(r'(?<=[!])(?=[^\s])', r' ', t1)
     t3 = re.sub(r' !', '', t2)
     t4 = re.sub(' +',' ', t3)
     out = re.sub(r' @ ', '', t4).replace('\n', '').replace('&amp;', '')
-    return out[-MAX_LEN:]
+    return out
 
-# Sample a character, with a given temperature/diversity parameter
+# Clean up output tweet (have to break URLs so twitter API doesnt complain about fake links)
+def clean_output_tweet(raw_text):
+    return re.sub('`|~', '', raw_text).replace('://', ':/') \
+                                       .replace('amp', '&') \
+                                       .replace(' : ', get_time(long=True)) \
+                                       .replace('at pm', 'at ' + get_time() + 'pm') \
+                                       .replace('at am', 'at ' + get_time() + 'am')
+
+# Sample a character, with a given temperature (diversity) parameter
 def sample(preds, temperature=1.0):
     preds = np.asarray(preds).astype('float64')
     preds = np.log(preds) / temperature
@@ -35,34 +45,39 @@ def sample(preds, temperature=1.0):
 
 # What is trump gonna say next!
 def generate_output_tweet(model, seed_tweet, char_indices, indices_char):
-    sentence_trunc = seed_tweet[-MAX_LEN:]
-    generated = ''
-    next_char = ''
-    diversity = random.uniform(DIVERSITY_RANGE[0], DIVERSITY_RANGE[1])
-    print(sentence_trunc)
-    while next_char != '`' or len(generated) < 2:
-        x_pred = np.zeros((1, MAX_LEN, NUM_CHARS))
+    sentence_trunc = seed_tweet[-MODEL_INPUT_LEN:]
+    generated, next_char = '', ''
+    diversity = random.uniform(MIN_TEMP, MAX_TEMP)
+    while (next_char != '`' or len(generated) < 2) and len(generated) < 500:
+        x_pred = np.zeros((1, MODEL_INPUT_LEN, NUM_CHARS))
         for t, char in enumerate(sentence_trunc):
             x_pred[0, t, char_indices[char]] = 1.
 
         preds = model.predict(x_pred, verbose=0)[0]
         next_index = sample(preds, diversity)
         next_char = indices_char[str(next_index)]
-
         generated += next_char
         sentence_trunc = sentence_trunc[1:] + next_char
-    # Get rid of end of tweet/start of tweet char, and break links so twitter doesnt complain
-    generated = re.sub('`|~', '', generated).replace('://', ':/') \
-                                            .replace(' : ', get_time()) \
-                                            .replace('amp', '&')
-    if len(generated) >= CHARACTER_LIMIT:
-        return split_tweet(generated)
+
+    # Clean up output text a bit
+    clean = clean_output_tweet(generated)
+    if len(clean) >= CHARACTER_LIMIT:
+        return split_tweet(clean)
     else:
-        return [generated]
+        return [clean]
 
 # I removed numbers as possible characters to simplify model - add them back in!
-def get_time():
-    return random.choice(POSSIBLE_HOURS) + ':' + random.choice(POSSIBLE_MINUTES) + ' '
+def get_time(long=False):
+    if long is False:
+        return random.choice(POSSIBLE_HOURS)
+    else:
+        return random.choice(POSSIBLE_HOURS) + ':' + random.choice(POSSIBLE_MINUTES) + ' '
+
+def id_or_none(tweet_tuple):
+    if tweet_tuple[0] is None:
+        return None
+    else:
+        return int(tweet_tuple[0])
 
 # Split tweets longer than 280 characters into smaller tweets, separate with ...
 def split_tweet(tweet_text):
@@ -74,58 +89,61 @@ def split_tweet(tweet_text):
     return out
 
 # Check for new tweets!
-def get_new_tweets(conn, api):
-    cur = conn.cursor()
+def get_new_tweets(cur, api):
     cur.execute("CREATE TABLE IF NOT EXISTS last_tweet_db (tweet_id BIGINT)")
     cur.execute("SELECT max(tweet_id) as tweet_id FROM last_tweet_db")
-    last_tweet = cur.fetchone()
-    api_tweets = api.GetUserTimeline(screen_name="realDonaldTrump")
-    if last_tweet[0] is not None:
-        if int(last_tweet[0]) == api_tweets[0].id:
-            cur.close()
-            return None
-    cur.execute("INSERT INTO last_tweet_db VALUES (%s)", (api_tweets[0].id,))
-    conn.commit()
-    cur.close()
-    input_tweet = '~' + api_tweets[0].text + '`'
-    i = 1
-    while len(input_tweet) < 200:
-        input_tweet = '~' + api_tweets[i].text + '`' + input_tweet
-        i += 1
-    return input_tweet
+    last_tweet = id_or_none(cur.fetchone())
+    api_tweets = api.GetUserTimeline(screen_name='realDonaldTrump', since_id=last_tweet)
+    if len(api_tweets) == 0:
+        return None
+    else:
+        cur.execute("INSERT INTO last_tweet_db VALUES (%s)", (api_tweets[0].id,))
+        all_tweets = []
+        for i in range(len(api_tweets)):
+            tweet = '~' + preprocess_input_tweet(api_tweets[i].text) + '`'
+            # Model needs `MODEL_INPUT_LEN` seed characters to make decent tweets
+            if len(tweet) < MODEL_INPUT_LEN:
+                tweet = BASE_SEED_TWEET + tweet
+            all_tweets.append(tweet[-MODEL_INPUT_LEN:])
+        return all_tweets
 
 def main():
     # Connect to Twitter API
-    api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'),
-                      consumer_secret=os.environ.get('CONSUMER_KEY_SECRET'),
-                      access_token_key=os.environ.get('ACCESS_TOKEN'),
-                      access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET'))
+    api = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
+                      consumer_secret=os.environ['CONSUMER_KEY_SECRET'],
+                      access_token_key=os.environ['ACCESS_TOKEN'],
+                      access_token_secret=os.environ['ACCESS_TOKEN_SECRET']))
 
     # Connect to Postgres
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
 
-    # Get latest tweet(s)
-    latest_tweet = get_new_tweets(conn, api)
-    if latest_tweet is None:
+    latest_tweets = get_new_tweets(cur, api)
+
+    # Done with Postgres - commit changes to DB and close cursor
+    conn.commit()
+    cur.close()
+
+    if latest_tweets is None:
         print("NO NEW TWEETS")
         sys.exit(1)
 
-    # Load character dictionaries
+    # These dictionaries map model predictions <-> characters
     char_indices = json.load(open('char_indices.json'))
     indices_char = json.load(open('indices_char.json'))
 
     # Load model: 2 layer LSTM, 512 units each, dropout = 0.2, RMSprop optimizer
-    # Trained on semi-redundant (5 char stride) 140 character long tweet chunks
+    # trained on semi-redundant (5 char stride) 140 character long tweet chunks
     model = load_model('model.h5')
 
-    # Generate tweet(s)
-    tweet_formatted = preprocess_input_tweet(latest_tweet)
-    output_tweets = generate_output_tweet(model, tweet_formatted, char_indices, indices_char)
-
     # Post tweet(s)
-    for tweet in output_tweets:
-        api.PostUpdate(tweet)
+    for input_tweet in latest_tweets:
+        print('INPUT: ' + input_tweet)
+        output_tweets = generate_output_tweet(model, input_tweet, char_indices, indices_char)
+        print(output_tweets)
+        for tweet in output_tweets:
+            api.PostUpdate(tweet)
 
 if __name__ == '__main__':
     main()
